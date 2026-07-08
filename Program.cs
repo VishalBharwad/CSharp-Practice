ad08fb999b9b53b72a4eadd4ad1dbac4cff3030f
VishalBharwad <bharvadvishal005@gmail.com>
2026-07-08 17:27:34 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
